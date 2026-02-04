78af90dddea8b91e45c1e0f643948aa4186cb361
bhagabanpaul62 <160720125+bhagabanpaul62@users.noreply.github.com>
2026-02-04 18:58:12 +0530

    robote

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,11 +1,8 @@
 export default function robots() {
-  const baseUrl = "https://officelookup.com";
-
   return {
     rules: {
       userAgent: "*",
       disallow: "/",
     },
-    sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
